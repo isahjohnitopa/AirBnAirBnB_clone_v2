f98387d1b4b95f06fc612e5f334177c68ce35fd9
isahjohnitopa <ijohnabdulmalik@gmail.com>
2023-11-21 11:48:10 -0800

    DBStorage - States and Cities

--- models/__init__.py
+++ models/__init__.py
@@ -2,6 +2,15 @@
 """This module instantiates an object of class FileStorage"""
 from models.engine.file_storage import FileStorage
 
+from os import getenv
 
-storage = FileStorage()
+storage_type = ('HBNB_TYPE_STORAGE')
+
+if storage_type == 'db':
+    from models.engine.db_storage import DBStorage
+    storage = DBStorage()
+else:
+    from models.engine.file_storage import FileStorage
+    storage = FileStorage()
+
 storage.reload()
